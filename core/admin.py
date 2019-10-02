d3e07a2c3735c8470d7bc5426ff8048be5fc6b0c
flavio322 <55261142+flavio322@users.noreply.github.com>
2019-10-01 21:12:15 -0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -34,4 +34,14 @@ class AnuncioAdmin(admin.ModelAdmin):
     list_display = ('cliente', 'texteTitulo', )
 admin.site.register(Anuncio, AnuncioAdmin)
 
-
+from django.contrib import admin
+from .models import  *
+ class AlunoAdmin(admin.ModelAdmin):
+     list_display = ('nome','matricula','data_nascimento')
+     class EmprestimoAdmin(admin.ModelAdmin):
+         list_display = ('data_devolucao','data_retirada','aluno','devolvido')
+         filter_horizontal = ['Livros']
+admin.site.register(Livro)
+admin.site.register(Autor)
+admin.site.register(Aluno,AlunoAdmin)
+admin.site.register(Emprestimo,EmprestimoAdmin)
